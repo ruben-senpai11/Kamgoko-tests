f71db1aef63af2ccfc362cbfda3b5f6d6b9561d9
ruben-senpai <albanhonfovou@gmail.com>
2025-02-05 12:02:57 +0100

    002

--- app-dashboard/src/components/payment.tsx
+++ app-dashboard/src/components/payment.tsx
@@ -18,7 +18,7 @@ export default function Payment({ label, date, amount }: Props) {
                 <use xlinkHref="#image0_615_271" transform="scale(0.015625)" />
               </pattern>
               <image id="image0_615_271" width="64" height="64" xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAAXNSR0IArs4c6QAAAONJREFUeF7t20EOhEAIRFG4/6F7DvEnYeFzryQIv6pBd2behOu9dPvsbog+k+NLgArQAqmJcw9iAAhSgZKB3IJkkAySQTJ4CiE+gA8oBeg0mH3Ai084P89HhqwEqIA209ICsQdjAeaZIgaAYKxBDMCAYy8fXwAIgiAIcoJpJEYGI4VjB3YrbC9gL2AvkCB43cM5PgZgAAZgQFnNZAhdGykQBEEQBEEQDBmgAm2glM/z+QUYisYUGoldO7kY32IEAzCg6RgIRgjFAsw+AgRBMNYgBmCAT2TCYfoPPz/HCqQCX1eBHzHnv7C7WhBSAAAAAElFTkSuQmCC" />
-            </defs>
+            </defs> 
           </svg>
           </span>
           <div className="">
